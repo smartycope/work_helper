7245ab161b3baa0e9fc0b61ad098f00d8d036403
Copeland Carter <smartycope@gmail.com>
2024-12-24 11:08:58 -0600

    Keeps a newline at the end of the notes always

--- work_helper.py
+++ work_helper.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import re
 from pathlib import Path
 from textual.app import App, ComposeResult
 from textual.reactive import reactive
@@ -87,28 +88,55 @@ class Steps:
 
     add_step = 'Add Step'
 
+_EXCLUDE = ("home,ctrl+a", "ctrl+w", "ctrl+f", "delete,ctrl+d",)
 class CustomTextArea(TextArea):
-    BINDINGS = [b for b in TextArea.BINDINGS if b.key != "home,ctrl+a"] + [
+    # Modified from https://textual.textualize.io/widgets/text_area/#textual.widgets._text_area.TextArea.BINDINGS
+    # TODO: most of these don't seem to work (anything with alt seems to get intercepted by Konsole)
+    BINDINGS = [
+        Binding("up,alt+k", "cursor_up", "Cursor up", show=False),
+        Binding("down,alt+l", "cursor_down", "Cursor down", show=False),
+        Binding("left,alt+j", "cursor_left", "Cursor left", show=False),
+        Binding("right,alt+;", "cursor_right", "Cursor right", show=False),
+        Binding("ctrl+left,ctrl+alt+j", "cursor_word_left", "Cursor word left", show=False),
+        Binding("ctrl+right,ctrl+alt+;", "cursor_word_right", "Cursor word right", show=False),
+        Binding("home,alt+h","cursor_line_start","Cursor line start",show=False),
+        Binding("end,alt+'", "cursor_line_end", "Cursor line end", show=False),
+        Binding("pageup", "cursor_page_up", "Cursor page up", show=False),
+        Binding("pagedown", "cursor_page_down", "Cursor page down", show=False),
+        Binding("ctrl+shift+left,ctrl+shift+alt+j", "cursor_word_left(True)", "Cursor left word select", show=False),
+        Binding("ctrl+shift+right,ctrl+shift+alt+;", "cursor_word_right(True)", "Cursor right word select", show=False),
+        Binding("shift+home,shfit+alt+h", "cursor_line_start(True)", "Cursor line start select", show=False),
+        Binding("shift+end,shfit+alt+'", "cursor_line_end(True)", "Cursor line end select", show=False),
+        Binding("shift+up,shift+alt+k", "cursor_up(True)", "Cursor up select", show=False),
+        Binding("shift+down,shift+alt+l", "cursor_down(True)", "Cursor down select", show=False),
+        Binding("shift+left,shift+alt+j", "cursor_left(True)", "Cursor left select", show=False),
+        Binding("shift+right,shift+alt+;", "cursor_right(True)", "Cursor right select", show=False),
+        Binding("ctrl+l", "select_line", "Select line", show=False),
+        Binding("backspace", "delete_left", "Delete character left", show=False),
+        Binding("ctrl+x", "cut", "Cut", show=False),
+        Binding("ctrl+c", "copy", "Copy", show=False),
+        Binding("ctrl+v", "paste", "Paste", show=False),
+        # Binding("ctrl+u", "delete_to_start_of_line", "Delete to line start", show=False),
+        # Binding("ctrl+k", "delete_to_end_of_line_or_delete_line", "Delete to line end", show=False),
+        Binding("ctrl+shift+d", "delete_line", "Delete line", show=False),
+        Binding("ctrl+z", "undo", "Undo", show=False),
+        Binding("ctrl+y,ctrl+shift+z", "redo", "Redo", show=False),
         Binding('ctrl+a', 'select_all', 'Select All', show=False),
-        Binding('alt+j', 'cursor_left', 'Cursor Left', show=False),
-        Binding('alt+l', 'cursor_down', 'Cursor Down', show=False),
-        Binding('alt+;', 'cursor_right', 'Cursor Right', show=False),
-        Binding('alt+k', 'cursor_up', 'Cursor Up', show=False),
-        Binding('esc,ctrl+b', 'unfocus', 'Unfocus', show=False),
-        Binding("home","cursor_line_start","Cursor line start",show=False),
+        Binding('esc', 'blur', 'Unfocus', show=False),
+        Binding("ctrl+backspace", "delete_word_left", "Delete left to start of word", show=False),
+        Binding("delete,shift+backspace", "delete_right", "Delete character right", show=False),
+        Binding("ctrl+delete,ctrl+shift+backspace", "delete_word_right", "Delete right to start of word", show=False),
     ]
 
     def __init__(self, ref):
         super().__init__(ref + '\n', id='textarea_' + ref)
         self.cursor_blink = False
 
-    # TODO: This doesn't work
-    def unfocus(self):
-        self.parent.focus()
-
-    # def watch_text(self, *args):
-    #     super().watch_text(*args)
-    #     self.scroll_to(None, 1000, animate=False)
+    def on_text_area_changed(self, event):
+        # Always keep a single newline at the end of notes
+        if re.match(r'(?:(?:.|\n))+(?:\s){2}\Z', self.text):
+            self.text = self.text.strip() + '\n'
+            self.move_cursor(self.document.end, select=False)
 
 class Sidebar(VerticalGroup):
     serial = reactive('')
@@ -150,7 +178,7 @@ class Sidebar(VerticalGroup):
         button.can_focus = False
         yield button
 
-        yield Label('TODO context:')
+        yield Label('TODO:')
         ta = TextArea(id='lower-sidebar')
         ta.can_focus = False
         yield ta
