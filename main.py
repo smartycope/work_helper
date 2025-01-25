0bc34dd5101dff0fb8280e2ea8b8a6a964d3a9a8
Copeland Carter <smartycope@gmail.com>
2025-01-24 21:18:25 -0600

    added order part keybaord shortcut v1

--- main.py
+++ main.py
@@ -2,16 +2,29 @@ import argparse
 
 from textual.containers import *
 from textual.widgets import *
+from time import sleep
 
 from HelperApp import HelperApp
+
+import keyboard
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--debug', action='store_true')
 args = parser.parse_args()
 
+def order_part_seq():
+    """ enter, tab x3, "NA", tab, "NA", tab x2, enter """
+    for key in ('enter', 'tab', 'tab', 'tab', 'N', 'A', 'tab', 'N', 'A', 'tab', 'tab', 'enter'):
+        keyboard.press_and_release(key)
+        if key != 'N':
+            sleep(.01)
+
+
 if __name__ == "__main__":
     app = HelperApp(debug=args.debug)
     try:
+        keyboard.add_hotkey('ctrl+shift+k', order_part_seq)
         app.run()
     finally:
         app.action_save()
+        keyboard.remove_all_hotkeys()
